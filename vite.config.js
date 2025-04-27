3f1851b6c36ce95b307b0f72f8317890142d6b23
unknown <albreto.camachojr01@gmail.com>
2025-04-26 23:00:11 -0400

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: "terser",
+    minify: "esbuild", // Using esbuild instead of terser
     rollupOptions: {
       output: {
         manualChunks: {
